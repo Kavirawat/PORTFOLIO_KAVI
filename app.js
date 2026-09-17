c400e0cfbee98e60788a2093f8cac89bd77348b1
Kavi rawat <kavirrawat896@gmail.com>
2026-09-17 16:38:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -100,11 +100,11 @@ const activePortfolio = () => {
 };
 
 arrowRight.addEventListener("click", () => {
-  if (index < 4) {
+  if (index < 5) {
     index++;
     arrowLeft.classList.remove("disabled");
   } else {
-    index = 5;
+    index = 6;
     arrowRight.classList.add("disabled");
   }
   activePortfolio();
